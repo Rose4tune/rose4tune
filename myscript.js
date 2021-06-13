70a5b183091a742395a0c93fd73807f920f4accd
Lee Ye Seo <81802112+Rose4tune@users.noreply.github.com>
2021-06-13 16:24:55 +0900

    create lodingpage

---
+++ myscript.js
@@ -0,0 +1,20 @@
+
+
+// console.log("hello!!");
+
+$('.loding').hide;
+
+
+
+// function pageLoding(){
+//     var loding = document.getElementsByClassName('loding');
+
+//     loding.style.display = 'none';
+// }
+
+// window.onload = function(){
+//     setTimeout(function(){
+//         var t = performance.timing;
+//         console.log(t.loadEventEnd - t.responseEnd);
+//     }, 0)
+// }
