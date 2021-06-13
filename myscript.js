142cdfa1b42af83701b551f8c1a1095636d9d0b8
Lee Ye Seo <81802112+Rose4tune@users.noreply.github.com>
2021-06-13 17:18:25 +0900

    java test

--- myscript.js
+++ myscript.js
@@ -1,20 +1,9 @@
 
 
-// console.log("hello!!");
+console.log("hello!!");
 
-$('.loding').hide;
-
-
-
-// function pageLoding(){
-//     var loding = document.getElementsByClassName('loding');
-
-//     loding.style.display = 'none';
-// }
-
-// window.onload = function(){
-//     setTimeout(function(){
-//         var t = performance.timing;
-//         console.log(t.loadEventEnd - t.responseEnd);
-//     }, 0)
-// }
+function pageLoding(){
+    var loding = document.getElementsByClassName('loding');
+    loding.style.display = 'none';
+    console.log('here');
+}
