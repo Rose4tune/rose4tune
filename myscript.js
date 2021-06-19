dd4d0f502495015217d7137b1c7b4b7e8c532db1
Lee Ye Seo <81802112+Rose4tune@users.noreply.github.com>
2021-06-19 17:04:44 +0900

    header, main

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
-function pageLoding(){
-    var loding = document.getElementsByClassName('loding')[0];
-    loding.style.display="none"
-    console.log('here!');
-}
+// function pageLoding(){
+//     var loding = document.getElementsByClassName('loding')[0];
+//     loding.style.display="none"
+//     console.log('here!');
+// }
