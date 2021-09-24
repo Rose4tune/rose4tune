d9cf45b554de31658ccf8ae2b85e53467dd08d71
Lee Ye Seo <rosefor2ne@gmail.com>
2021-09-24 15:43:23 +0900

    모달 띄울 때 바탕화면 스크롤 제한

--- js/modal.js
+++ js/modal.js
@@ -1,30 +1,27 @@
 const modal = document.getElementsByClassName("modal")[0]
 function modalOn() {
   modal.style.display = "flex";
-  $('.modal').on('scroll touchmove mousewheel', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  });
+  $("html, body").addClass("not_scroll");
+
 };
 function isModalOn() {
   return modal.style.display === "flex";
 };
 function modalOff() {
   modal.style.display = "none";
-  $('.modal').off('scroll touchmove mousewheel');
+  $("html, body").removeClass("not_scroll");
 };
-const btnModal = document.getElementsByClassName("btn-modal")[0]
+const btnModal = document.getElementsByClassName("btn_modal")[0]
 btnModal.addEventListener("click", e => {
   modalOn();
 });
-const closeBtn = modal.querySelector(".btn_close")
+const closeBtn = modal.querySelector(".btn_close");
 closeBtn.addEventListener("click", e => {
   modalOff();
 });
 modal.addEventListener("click", e => {
   const evTarget = e.target
-  if (evTarget.classList.contains("modal-overlay")) {
+  if (evTarget.classList.contains("modal_overlay")) {
     modalOff()
   };
 });
