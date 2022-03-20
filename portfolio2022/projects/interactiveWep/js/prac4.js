b78caabe8d2cbadacd60860032a986db6bde4838
Lee Ye Seo <rosefor2ne@gmail.com>
2022-03-20 14:50:18 +0900

    배경 패럴렉스 기초(layered cloud 속도 다른 2집단)

--- portfolio2022/projects/interactiveWep/js/prac4.js
+++ portfolio2022/projects/interactiveWep/js/prac4.js
@@ -1,15 +1,18 @@
 let scrollTop, bar;
+let cloudWrap, cloudWrap2;
 
 window.onload = function(){
   bar = document.getElementById('bar');
+  cloudWrap = document.getElementsByClassName('cloudWrap')[0];
+  cloudWrap2 = document.getElementsByClassName('cloudWrap')[1];
 }
 
 window.addEventListener("scroll", function(e){
-  // scrollTop = this.document.documentElement.scrollTop;
-  scrollTop = this.scrollY;
+  scrollTop = this.scrollY; //= this.document.documentElement.scrollTop;
 
   let per = Math.ceil(scrollTop / (this.document.body.scrollHeight - this.outerHeight) * 100);
-  // console.log(scrollTop, per)
-
   bar.style.height = per + "%";
+
+  cloudWrap.style.transform = `translate(0, ${-scrollTop/2}px)`;
+  cloudWrap2.style.transform = `translate(0, ${-scrollTop/1.4}px)`;
 }, false)
