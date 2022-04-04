b4a60650962c92d3ca05bb4ad9d250a0aa9035ed
Lee Ye Seo <rosefor2ne@gmail.com>
2022-04-04 16:59:02 +0900

    jsDoc 연습

---
+++ portfolio2022/projects/typeScript/getting-start/jsTest.js
@@ -0,0 +1,28 @@
+var user = {};
+
+//@ts-check
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+// function fetchUser() {
+//   return axios.get(url);
+// }
+
+// fetchUser().then(function (response){
+//   response.add
+// })
